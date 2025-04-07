583c4d93d7c201ebf9f4d05d926f75306fb9e560
Denis Issoupov <denis@ekspand.com>
2025-04-07 06:34:13 +0400

    Refactor Enums and Display Names

--- internal/enumgen/tools_test.go
+++ internal/enumgen/tools_test.go
@@ -16,20 +16,20 @@ func Test_FormatDisplayName(t *testing.T) {
 	assert.Equal(t, "EC2 Instance", FormatDisplayName("EC2Instance"))
 }
 
-func Test_EnumDisplayName(t *testing.T) {
+func Test_EnumDisplayValue(t *testing.T) {
 	rt := e2e.ResourceType_EC2Instance | e2e.ResourceType_S3Bucket | e2e.ResourceType_LambdaFunction
-	assert.Equal(t, "EC2 Instance,S3 Bucket,Lambda Function", rt.DisplayName())
-	assert.Equal(t, []string{"EC2 Instance", "S3 Bucket", "Lambda Function"}, rt.DisplayNames())
+	assert.Equal(t, "EC2 Instance,S3 Bucket,Lambda Function", rt.DisplayValue())
+	assert.Equal(t, []string{"EC2 Instance", "S3 Bucket", "Lambda Function"}, rt.DisplayValues())
 
 	rt = 0
-	assert.Equal(t, "", rt.DisplayName())
-	assert.Nil(t, rt.DisplayNames())
+	assert.Equal(t, "Unknown", rt.DisplayValue())
+	assert.Equal(t, []string{"Unknown"}, rt.DisplayValues())
 
 	rt = e2e.ResourceType_S3Bucket
-	assert.Equal(t, "S3 Bucket", rt.DisplayName())
-	assert.Equal(t, []string{"S3 Bucket"}, rt.DisplayNames())
+	assert.Equal(t, "S3 Bucket", rt.DisplayValue())
+	assert.Equal(t, []string{"S3 Bucket"}, rt.DisplayValues())
 
 	rt = e2e.ResourceType_All
-	assert.Equal(t, "EC2 Instance,S3 Bucket,Lambda Function", rt.DisplayName())
-	assert.Equal(t, []string{"EC2 Instance", "S3 Bucket", "Lambda Function"}, rt.DisplayNames())
+	assert.Equal(t, "EC2 Instance,S3 Bucket,Lambda Function", rt.DisplayValue())
+	assert.Equal(t, []string{"EC2 Instance", "S3 Bucket", "Lambda Function"}, rt.DisplayValues())
 }
